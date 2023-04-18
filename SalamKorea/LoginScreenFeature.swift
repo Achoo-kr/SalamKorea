e53bed764b5043d7af8748a38d97bffa952eea92
Choo Hyun Ho <hyunho4178@gmail.com>
2023-04-19 06:40:57 +0900

    Move types into production

---
+++ SalamKorea/LoginScreenFeature.swift
@@ -0,0 +1,50 @@
+//
+//  LoginScreenFeature.swift
+//  SalamKorea
+//
+//  Created by 추현호 on 2023/04/19.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct LoginScreenFeature: ReducerProtocol {
+    struct State: Equatable {
+        var emailText: String = ""
+        var passwordText: String = ""
+        var errorMessage: String?
+    }
+    
+    enum Action {
+        case onEmailTextFieldChanged(text: String)
+        case onPasswordTextFieldChanged(text: String)
+        case onLoginButtonTapped
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+            
+        case let .onEmailTextFieldChanged(text):
+            state.emailText = text
+            
+            if !text.isEmpty {
+                state.errorMessage = nil
+            }
+            
+            return .none
+            
+        case let .onPasswordTextFieldChanged(text):
+            state.passwordText = text
+            
+            if !text.isEmpty {
+                state.errorMessage = nil
+            }
+            
+            return .none
+        
+        case .onLoginButtonTapped:
+            state.errorMessage = "Email and password should not be empty"
+            return .none
+        }
+    }
+}
